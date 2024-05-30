8daa1b725266a5a519bdab711186288bf22c5652
Hao <nguyenquanghao1808@gmail.com>
2024-05-30 10:49:30 +0700

    Config URL

--- public/src/utils/ApiRoutes.js
+++ public/src/utils/ApiRoutes.js
@@ -1,4 +1,8 @@
-export const host = "http://localhost:5000";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const host = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_HOST : process.env.REACT_APP_DEV_HOST;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
