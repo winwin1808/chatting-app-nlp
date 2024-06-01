b998132278ff76a1307b3db316dec00923352870
Hao <nguyenquanghao1808@gmail.com>
2024-06-01 14:16:37 +0700

    Adjust vercel

--- public/src/components/Contact.jsx
+++ public/src/components/Contact.jsx
@@ -6,12 +6,16 @@ export default function Contacts({ contacts, changeChat }) {
   const [currentUserName, setCurrentUserName] = useState(undefined);
   const [currentUserImage, setCurrentUserImage] = useState(undefined);
   const [currentSelected, setCurrentSelected] = useState(undefined);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = JSON.parse(localStorage.getItem('register-user'));
-        setCurrentUserName(data.username);
-        setCurrentUserImage(data.avatarImage);
+        const storedData = localStorage.getItem('register-user');
+        if (storedData) {
+          const data = JSON.parse(storedData);
+          setCurrentUserName(data.username);
+          setCurrentUserImage(data.avatarImage);
+        }
       } catch (error) {
         // Handle error
         console.error('Error fetching data:', error);
@@ -25,35 +29,33 @@ export default function Contacts({ contacts, changeChat }) {
     setCurrentSelected(index);
     changeChat(contact);
   };
+
   return (
     <>
-      {currentUserImage && currentUserImage && (
+      {currentUserName && currentUserImage && (
         <Container>
           <div className="brand">
             <img src={Logo} alt="logo" />
             <h3>Chatting</h3>
           </div>
           <div className="contacts">
-            {contacts.map((contact, index) => {
-              return (
-                <div
-                  key={contact._id}
-                  className={`contact ${index === currentSelected ? "selected" : ""
-                    }`}
-                  onClick={() => changeCurrentChat(index, contact)}
-                >
-                  <div className="avatar">
-                    <img
-                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
-                      alt=""
-                    />
-                  </div>
-                  <div className="username">
-                    <h3>{contact.username}</h3>
-                  </div>
+            {contacts.map((contact, index) => (
+              <div
+                key={contact._id}
+                className={`contact ${index === currentSelected ? "selected" : ""}`}
+                onClick={() => changeCurrentChat(index, contact)}
+              >
+                <div className="avatar">
+                  <img
+                    src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                    alt=""
+                  />
                 </div>
-              );
-            })}
+                <div className="username">
+                  <h3>{contact.username}</h3>
+                </div>
+              </div>
+            ))}
           </div>
           <div className="current-user">
             <div className="avatar">
@@ -71,39 +73,46 @@ export default function Contacts({ contacts, changeChat }) {
     </>
   );
 }
+
 const Container = styled.div`
   display: grid;
   grid-template-rows: 15% 70% 15%;
-  
   overflow: hidden;
   background-color: #B63E3E;
+
   .brand {
     display: flex;
     align-items: center;
     gap: 1rem;
     justify-content: center;
+
     img {
       height: 2rem;
     }
+
     h3 {
       color: white;
       text-transform: uppercase;
     }
   }
+
   .contacts {
     display: flex;
     flex-direction: column;
     align-items: center;
     overflow: auto;
     gap: 0.8rem;
+
     &::-webkit-scrollbar {
       width: 0.2rem;
+
       &-thumb {
         background-color: #ffffff39;
         width: 0.1rem;
         border-radius: 1rem;
       }
     }
+
     .contact {
       background-color: #ffffff34;
       min-height: 4rem;
@@ -115,11 +124,13 @@ const Container = styled.div`
       gap: 1rem;
       align-items: center;
       transition: 0.5s ease-in-out;
+
       .avatar {
         img {
           height: 3rem;
         }
       }
+
       .username {
         h3 {
           font-size: 1.2rem;
@@ -127,6 +138,7 @@ const Container = styled.div`
         }
       }
     }
+
     .selected {
       background-color: #5F1D1D;
     }
@@ -138,20 +150,24 @@ const Container = styled.div`
     padding: 1.3rem;
     align-items: center;
     gap: 2rem;
+
     .avatar {
       img {
         height: 3rem;
         max-inline-size: 100%;
       }
     }
+
     .username {
       h3 {
         font-size: 1.2rem;
         color: white;
       }
     }
+
     @media screen and (min-width: 720px) and (max-width: 1080px) {
       gap: 0.5rem;
+
       .username {
         h3 {
           font-size: 1rem;
